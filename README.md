8d5269daf30e6467c490315e3d9264eda50c7c83
shoko <shoko@shokopc01.local>
2020-07-12 19:16:14 +0900

    add test for AAD jwt token

---
+++ README.md
@@ -0,0 +1,3 @@
+目的
+- Microsoftのトークン構造確認
+- auth0:java-jwtの確認
---
+++ src/test/kotlin/com/example/jwttest/jwtTest.kt
@@ -0,0 +1,36 @@
+package com.example.jwttest
+
+import com.auth0.jwt.JWT
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class jwtTest {
+
+    @Test
+    fun jwtTest() {
+        // Microsoft sample token
+        // https://docs.microsoft.com/ja-jp/azure/active-directory/develop/id-tokens
+        val token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjFMVE16YWtpaGlSbGFfOHoyQkVKVlhlV01xbyJ9.eyJ2ZXIiOiIyLjAiLCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vOTEyMjA0MGQtNmM2Ny00YzViLWIxMTItMzZhMzA0YjY2ZGFkL3YyLjAiLCJzdWIiOiJBQUFBQUFBQUFBQUFBQUFBQUFBQUFJa3pxRlZyU2FTYUZIeTc4MmJidGFRIiwiYXVkIjoiNmNiMDQwMTgtYTNmNS00NmE3LWI5OTUtOTQwYzc4ZjVhZWYzIiwiZXhwIjoxNTM2MzYxNDExLCJpYXQiOjE1MzYyNzQ3MTEsIm5iZiI6MTUzNjI3NDcxMSwibmFtZSI6IkFiZSBMaW5jb2xuIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiQWJlTGlAbWljcm9zb2Z0LmNvbSIsIm9pZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC02NmYzLTMzMzJlY2E3ZWE4MSIsInRpZCI6IjkxMjIwNDBkLTZjNjctNGM1Yi1iMTEyLTM2YTMwNGI2NmRhZCIsIm5vbmNlIjoiMTIzNTIzIiwiYWlvIjoiRGYyVVZYTDFpeCFsTUNXTVNPSkJjRmF0emNHZnZGR2hqS3Y4cTVnMHg3MzJkUjVNQjVCaXN2R1FPN1lXQnlqZDhpUURMcSFlR2JJRGFreXA1bW5PcmNkcUhlWVNubHRlcFFtUnA2QUlaOGpZIn0.1AFWW-Ck5nROwSlltm7GzZvDwUkqvhSQpm55TQsmVo9Y59cLhRXpvB8n-55HCr9Z6G_31_UbeUkoz612I2j_Sm9FFShSDDjoaLQr54CreGIJvjtmS3EkK9a7SJBbcpL1MpUtlfygow39tFjY7EVNW9plWUvRrTgVk7lYLprvfzw-CIqw3gHC-T7IK_m_xkr08INERBtaecwhTeN4chPC4W3jdmw_lIxzC48YoQ0dB1L9-ImX98Egypfrlbm0IBL5spFzL6JDZIRRJOu8vecJvj1mq-IUhGt0MacxX8jdxYLP-KUu2d9MbNKpCKJuZ7p8gwTL5B7NlUdh_dmSviPWrw"
+
+        val decodeToken = JWT.decode(token)
+        val subject = decodeToken.subject
+        println("[print subject] $subject")
+        val oid = decodeToken.claims.getValue("oid").asString()
+        println("[print oid] $oid")
+    }
+
+    @Test
+    fun jwtTest_emptyToken() {
+        val token = ""
+        try {
+            val decodeToken = JWT.decode(token)
+            println("[INFO] decodeToken: $decodeToken")
+        } catch (ex: Exception) {
+            println("[ERR] cannot decode token")
+            ex.printStackTrace()
+        }
+    }
+
+
+}
